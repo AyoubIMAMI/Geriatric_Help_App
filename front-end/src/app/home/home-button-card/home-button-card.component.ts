29ae20f6a1a8802de44d0071fda08577aaec9772
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2022-03-26 14:33:21 +0100

    Adding button card component on home page

---
+++ front-end/src/app/home/home-button-card/home-button-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-button-card',
+  templateUrl: './home-button-card.component.html',
+  styleUrls: []
+})
+export class HomeButtonCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- front-end/src/app/home/home.module.ts
+++ front-end/src/app/home/home.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import {HomeComponent} from "./home.component";
 import {CommonModule} from "@angular/common";
 import {LastQuizCardComponent} from "./last-quiz-card/last-quiz-card.component";
+import {HomeButtonCardComponent} from "./home-button-card/home-button-card.component";
 
 @NgModule({
   declarations: [
     HomeComponent,
+    HomeButtonCardComponent,
     LastQuizCardComponent
   ],
   imports: [CommonModule]
