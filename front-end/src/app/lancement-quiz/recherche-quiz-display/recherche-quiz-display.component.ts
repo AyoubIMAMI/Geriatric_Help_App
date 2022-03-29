5b11f0922aab4fd88927928ea8770e870eff22c2
MouradKarrakchou <mourad.karrakchou@gmail.com>
2022-03-29 11:10:25 +0200

    Gestion des quizz

--- front-end/src/app/lancement-quiz/recherche-quiz-display/recherche-quiz-display.component.ts
+++ front-end/src/app/lancement-quiz/recherche-quiz-display/recherche-quiz-display.component.ts
@@ -1,14 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {Quiz} from "../../../models/quiz.model";
+import {Router} from "@angular/router";
+import {QuizService} from "../../../services/quiz.service";
 @Component({
   selector: 'app-recherche-quiz-display',
   templateUrl: './recherche-quiz-display.component.html',
   styleUrls: ['./recherche-quiz-display.component.scss']
 })
+
 export class RechercheQuizDisplayComponent implements OnInit {
 
-  constructor() { }
+  public quizList: Quiz[] = [];
+
+  constructor(private router: Router, public quizService: QuizService) {
+    this.quizService.quizzes$.subscribe((quizzes: Quiz[]) => {
+      this.quizList = quizzes;
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  quizSelected(selected: boolean): void {
+    console.log('event received from child:', selected);
   }
 
 }
