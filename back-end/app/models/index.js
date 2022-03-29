bc9228c1b395a6375d70dc0e861e09ed314d283a
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2022-03-29 11:11:57 +0200

    backend create resident REST

--- back-end/app/models/index.js
+++ back-end/app/models/index.js
@@ -2,10 +2,13 @@ const Answer = require('./answer.model.js')
 const Question = require('./question.model.js')
 const Quiz = require('./quiz.model.js')
 const User = require('./user.model.js')
+const Resident = require('./resident.model.js')
+
 
 module.exports = {
-  Answer,
-  Question,
-  Quiz,
-  User,
+    Answer,
+    Question,
+    Quiz,
+    User,
+    Resident
 }
