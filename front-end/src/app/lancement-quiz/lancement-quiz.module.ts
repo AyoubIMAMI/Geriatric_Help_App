9e20e711b5b1ebd648c8391fa7686dd86f62fa5c
MouradKarrakchou <mourad.karrakchou@gmail.com>
2022-03-29 10:27:17 +0200

    Gestion des quizz

--- front-end/src/app/lancement-quiz/lancement-quiz.module.ts
+++ front-end/src/app/lancement-quiz/lancement-quiz.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {RouterModule} from "@angular/router";
-
+import {RechercheQuizDisplayComponent} from "./recherche-quiz-display/recherche-quiz-display.component";
+import {LancementQuizComponent} from "./lancement-quiz.component";
 
 @NgModule({
   declarations: [
+    LancementQuizComponent,
+    RechercheQuizDisplayComponent
   ],
 
   imports: [
@@ -12,4 +15,4 @@ import {RouterModule} from "@angular/router";
     RouterModule
   ]
 })
-export class StartQuizModule {}
+export class LancementQuizModule {}
---
+++ front-end/src/app/lancement-quiz/recherche-quiz-display/recherche-quiz-display.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-recherche-quiz-display',
+  templateUrl: './recherche-quiz-display.component.html',
+  styleUrls: ['./recherche-quiz-display.component.scss']
+})
+export class RechercheQuizDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
