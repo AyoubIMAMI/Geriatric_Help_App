cb4a627a904961621a90653566fde10d86e6ba54
MouradKarrakchou <mourad.karrakchou@gmail.com>
2022-03-29 11:37:04 +0200

    Gestion des quizz

---
+++ front-end/src/app/gestion-quiz/gestion-quiz.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-quiz',
+  templateUrl: './gestion-quiz.component.html',
+  styleUrls: ['./gestion-quiz.component.scss']
+})
+export class GestionQuizComponent implements OnInit {
+
+
+  constructor() {
+  }
+
+  ngOnInit(): void {}
+
+}
