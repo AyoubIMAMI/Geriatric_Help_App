f45d3777f9d4caf1997538d2ac039b6743eed539
MouradKarrakchou <mourad.karrakchou@gmail.com>
2022-04-02 10:10:22 +0200

    Creation d'un quiz

--- front-end/src/app/create-quiz/create-quiz.component.ts
+++ front-end/src/app/create-quiz/create-quiz.component.ts
@@ -20,24 +20,11 @@ export class CreateQuizComponent implements OnInit {
    */
   public quizForm: FormGroup;
 
-  constructor(public formBuilder: FormBuilder, public quizService: QuizService) {
-    this.quizForm = this.formBuilder.group({
-      name: [''],
-      theme: ['']
-    });
-    // You can also add validators to your inputs such as required, maxlength or even create your own validator!
-    // More information: https://angular.io/guide/reactive-forms#simple-form-validation
-    // Advanced validation: https://angular.io/guide/form-validation#reactive-form-validation
+  constructor() {
   }
 
   ngOnInit(): void {
   }
 
-  addQuiz(): void {
-    // We retrieve here the quiz object from the quizForm and we cast the type "as Quiz".
-    const quizToCreate: Quiz = this.quizForm.getRawValue() as Quiz;
-
-    this.quizService.addQuiz(quizToCreate);
-  }
 
 }
--- front-end/src/app/create-quiz/create-quiz.module.ts
+++ front-end/src/app/create-quiz/create-quiz.module.ts
@@ -3,10 +3,14 @@ import {CommonModule} from "@angular/common";
 import {RouterModule} from "@angular/router";
 import {CreateQuizComponent} from "./create-quiz.component";
 import {ReactiveFormsModule} from "@angular/forms";
+import {CreationOfQuizComponent} from "./creation-of-quiz/creation-of-quiz.component";
+import {AddingQuestionsComponent} from "./adding-questions/adding-questions.component";
 
 @NgModule({
   declarations: [
-    CreateQuizComponent
+    CreateQuizComponent,
+    CreationOfQuizComponent,
+    AddingQuestionsComponent
   ],
 
   imports: [
