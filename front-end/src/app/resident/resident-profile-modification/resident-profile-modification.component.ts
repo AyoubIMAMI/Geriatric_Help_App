d031ce83bff3befa28a4d05a6b05dd8408c1d323
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2022-04-05 10:40:59 +0200

    CA COMPILE !

---
+++ front-end/src/app/resident/resident-profile-modification/resident-profile-modification.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resident-profile-modification',
+  templateUrl: './resident-profile-modification.component.html',
+  styleUrls: []
+})
+export class ResidentProfileModificationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
