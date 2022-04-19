40948ebece07829578edb2e703a239d926391ca4
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2022-04-19 11:27:38 +0200

    Ajout stats sur backend

--- back-end/app/models/stat.model.js
+++ back-end/app/models/stat.model.js
@@ -3,7 +3,7 @@ const BaseModel = require('../utils/base-model.js')
 
 module.exports = new BaseModel('Stat', {
     residentId: Joi.string().required(),
-    quizzId: Joi.string.required(),
-    correctAwnswer: Joi.number.required(),
-    badAwnswer: Joi.number.required(),
+    quizzId: Joi.string().required(),
+    correctAwnswer: Joi.number().required(),
+    badAwnswer: Joi.number().required(),
 })
