7c1947f85d5bfcadf54d71eee21ced6491ca2a7d
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2022-04-19 11:15:50 +0200

    Ajout stat.model

---
+++ back-end/app/models/stat.model.js
@@ -0,0 +1,9 @@
+const Joi = require('joi')
+const BaseModel = require('../utils/base-model.js')
+
+module.exports = new BaseModel('Stat', {
+    residentId: Joi.string().required(),
+    quizzId: Joi.string.required(),
+    correctAwnswer: Joi.number.required(),
+    badAwnswer: Joi.number.required(),
+})
