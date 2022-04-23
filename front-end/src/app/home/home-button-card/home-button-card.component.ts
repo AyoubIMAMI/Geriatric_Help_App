8066fad1c4c093bf07200efd3356ce7eb9286b8f
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2022-04-23 13:23:45 +0200

    Mise à jour graphique des bouton du home

--- front-end/src/app/home/home-button-card/home-button-card.component.ts
+++ front-end/src/app/home/home-button-card/home-button-card.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-button-card',
   templateUrl: './home-button-card.component.html',
-  styleUrls: []
+  styleUrls: ['./home-button-card.component.scss']
 })
 export class HomeButtonCardComponent implements OnInit {
 
