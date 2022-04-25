ec8fc6ca91a4a82e2d1869673be23e56f98e68ac
MouradKarrakchou <mourad.karrakchou@gmail.com>
2022-04-25 18:17:12 +0200

    Creation d'un quiz

--- front-end/src/app/tutorial/tutorial.module.ts
+++ front-end/src/app/tutorial/tutorial.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {RouterModule} from "@angular/router";
 import {TutorialComponent} from "./tutorial.component";
-import { TutorielDisplayComponent } from './tutoriel-display/tutoriel-display.component';
 
 @NgModule({
   declarations: [
-    TutorialComponent,
-    TutorielDisplayComponent
+    TutorialComponent
   ],
   imports: [CommonModule, RouterModule]
 })
