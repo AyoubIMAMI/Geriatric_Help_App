265a9270e808b8ce13e70f89bbb27d61a34b87b2
MouradKarrakchou <mourad.karrakchou@gmail.com>
2022-04-25 18:50:13 +0200

    Creation d'un quiz

--- front-end/src/app/tutorial/tutorial.component.ts
+++ front-end/src/app/tutorial/tutorial.component.ts
@@ -14,17 +14,17 @@ export class TutorialComponent implements OnInit {
   public tutoList: Tutoriel[] = [];
 
   constructor(private router: Router, public quizService: QuizService) {
-    this.createTutoriel();
   }
 
   ngOnInit(): void {
+    this.createTutoriel();
   }
 
   createTutoriel(): void {
-    this.tutoList[0]=new class implements Tutoriel {
-      question: "De quel couleur";
-      reponse: "oui c'est comme ca";
-    };
+    this.tutoList[0]={
+      question: "De quel couleur",
+      reponse: "oui c'est comme ca"
+    }
     console.log(this.tutoList)
   }
 }
--- front-end/src/app/tutorial/tutorial.module.ts
+++ front-end/src/app/tutorial/tutorial.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {RouterModule} from "@angular/router";
 import {TutorialComponent} from "./tutorial.component";
+import {BrowserModule} from "@angular/platform-browser";
 
 @NgModule({
   declarations: [
     TutorialComponent
   ],
-  imports: [CommonModule, RouterModule]
+  imports: [CommonModule, RouterModule,BrowserModule]
 })
-export class QuizzesModule { }
+export class TutorialModule { }
