ca9e4c022a5fd45c240a47ce816330e7cb2e38a4
MouradKarrakchou <mourad.karrakchou@gmail.com>
2022-05-23 18:59:52 +0200

    fix on add and create quiz

---
+++ back-end/app/api/clickNumber/index.js
@@ -0,0 +1,49 @@
+const {Router} = require("express");
+const manageAllErrors = require("../../utils/routes/error-management");
+const {filterClickNumberByResident, filterClickNumberByDate} = require("./manager");
+const Joi = require("joi");
+const {ClickNumber} = require("../../models");
+
+const router = new Router()
+
+
+router.put('/:residentId/:annee/:mois/:jour', (req, res) => {
+    try {
+        let clickNumber=filterClickNumberByResident(req.params.residentId,req.params.annee,req.params.mois,req.params.jour)
+        console.log(clickNumber)
+        res.status(200).json(
+            ClickNumber.update(clickNumber.id,{
+            numberOfClicks:clickNumber.numberOfClicks+req.body.numberOfClicks,
+            numberOfPages:clickNumber.numberOfPages+1,
+            numberOfGoodResponses:clickNumber.numberOfGoodResponses+req.body.numberOfGoodResponses,
+            numberOfBadResponses:clickNumber.numberOfBadResponses+req.body.numberOfBadResponses,
+            })
+        )
+    } catch (err) {
+        manageAllErrors(res, err)
+    }
+})
+
+router.post('/:residentId', (req, res) => {
+    try {
+        const clickNumber = ClickNumber.create({ ...req.body})
+        res.status(201).json(clickNumber)
+    } catch (err) {
+        manageAllErrors(res, err)
+    }
+})
+
+router.get('/:residentId', (req, res) => {
+    date1=new Date(req.body.anneeA,req.body.moisA,req.body.jourA)
+    date2=new Date(req.body.anneeB,req.body.moisB,req.body.jourB)
+    try {
+        res.status(200).json(
+            filterClickNumberByDate(req.params.residentId,date1,date2)
+        )
+    } catch (err) {
+        manageAllErrors(res, err)
+    }
+})
+
+
+module.exports = router
